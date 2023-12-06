7dff46105a7547b30b90635a3efee9852f6707af
Ziyao Cheng <yottacheng3@gmail.com>
2023-12-06 00:58:07 +0000

    excersize 1

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Thing(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
-

6fee55c27c30a187c429c4516b121f637a15acdb
Ziyao Cheng <yottacheng3@gmail.com>
2023-12-06 01:20:58 +0000

    Add feature XYZ

--- things/models.py
+++ things/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from django.db import models
+from django.core.validators import MaxValueValidator
+
 class Thing(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(validators=[MaxValueValidator(100)])  # 添加验证规则
 
     def __str__(self):
         return self.name

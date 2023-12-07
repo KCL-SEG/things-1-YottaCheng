1e7d5bb5df31bdc9e093f8cff6b1f2f498f73dba
Ziyao Cheng <yottacheng3@gmail.com>
2023-12-07 23:11:06 +0000

    new

--- things/models.py
+++ things/models.py
@@ -1,11 +1,12 @@
 # 在 models.py 文件中
 from django.db import models
 from django.core.validators import MaxLengthValidator
+from django.core.validators import MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(blank=True, validators=[MaxLengthValidator(120)])
-    quantity = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
+    description = models.TextField(blank=True, validators=[MaxLengthValidator(120)])  # 限制最大长度为120
+    quantity = models.PositiveIntegerField(validators=[MaxValueValidator(100)])  # 添加验证规则
 
     def __str__(self):
         return self.name
